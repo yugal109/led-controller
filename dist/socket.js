291c388a62314e00c44c7fd5492f1b8fcb939a0d
Yugal <yugalkhati570@gmail.com>
2024-12-14 16:07:06 +0545

    fixed docker compose file

--- dist/socket.js
+++ dist/socket.js
@@ -2,14 +2,14 @@ export const registerSocketEvents = (io) => {
     // log name space
     const logNameSpace = io.of("/logs");
     logNameSpace.on("connection", (socket) => {
-        console.log(`Client connected: ${socket.id}`);
+        // console.log(`Client connected: ${socket.id}`);
         socket.on("joinRoom", (room) => {
             socket.join(room);
-            console.log(`${socket.id} joined room: ${room}`);
+            // console.log(`${socket.id} joined room: ${room}`);
         });
         // Handle disconnection
         socket.on("disconnect", () => {
-            console.log(`Client disconnected: ${socket.id}`);
+            // console.log(`Client disconnected: ${socket.id}`);
         });
     });
 };
--- src/socket.ts
+++ src/socket.ts
@@ -4,16 +4,16 @@ export const registerSocketEvents = (io: Server) => {
   // log name space
   const logNameSpace = io.of("/logs");
   logNameSpace.on("connection", (socket: Socket) => {
-    console.log(`Client connected: ${socket.id}`);
+    // console.log(`Client connected: ${socket.id}`);
 
     socket.on("joinRoom", (room) => {
       socket.join(room);
-      console.log(`${socket.id} joined room: ${room}`);
+      // console.log(`${socket.id} joined room: ${room}`);
     });
 
     // Handle disconnection
     socket.on("disconnect", () => {
-      console.log(`Client disconnected: ${socket.id}`);
+      // console.log(`Client disconnected: ${socket.id}`);
     });
   });
 };
